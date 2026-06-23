07ed7c1e1fcfd3473b427f6dbfa019afc801fe84
baum-knknkny <hutuuneko_mukiryoku@yahoo.co.jp>
2026-06-23 13:27:41 +0900

    Add files via upload

---
+++ Baum/js/storyA.js
@@ -0,0 +1,55 @@
+const storyList = [
+  {
+    id:"00001",
+    title:"0-1 突然の気づき",
+    chapter:0,
+    chapterName:"序章"
+  },
+  {
+    id:"00002",
+    title:"0-2 一つの決意",
+    chapter:0,
+    chapterName:"序章"
+  },
+  {
+    id:"00003",
+    title:"0-3 友としての関わり",
+    chapter:0,
+    chapterName:"序章"
+  },{
+    id:"00004",
+    title:"0-4 特別な知らせ",
+    chapter:0,
+    chapterName:"序章"
+  },{
+    id:"00005",
+    title:"0-5 ダイアゴン横丁",
+    chapter:0,
+    chapterName:"序章"
+  },{
+    id:"00006",
+    title:"0-6 幕開けの足音",
+    chapter:0,
+    chapterName:"序章"
+  },{
+    id:"00007",
+    title:"1-1 ホグワーツ特急",
+    chapter:1,
+    chapterName:"第1章"
+  },{
+    id:"00008",
+    title:"1-2 組み分け帽子",
+    chapter:1,
+    chapterName:"第1章"
+  },{
+    id:"00009",
+    title:"1-3 初めての授業",
+    chapter:1,
+    chapterName:"第1章"
+  },{
+    id:"00010",
+    title:"1-4 力の誇示",
+    chapter:1,
+    chapterName:"第1章"
+  }
+];
