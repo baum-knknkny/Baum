26e08f7efbedefb70119afe3ca7dcf8fbcfaaeb1
baum-knknkny <hutuuneko_mukiryoku@yahoo.co.jp>
2026-06-23 20:00:20 +0900

    Refactor functions for name handling and story generation

--- js/novel.js
+++ js/novel.js
@@ -124,7 +124,7 @@ async function openStory(id) {
     console.log("開こうとしてる:", id);
 
     const path =
-        `content/novel-a/${id}.md`;
+        `Baum/content/novel-a/${id}.md`;
 
     console.log("パス:", path);
 
