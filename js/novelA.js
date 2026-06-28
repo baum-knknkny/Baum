b789aa26eeecb530f66be546032502b243d61fb6
baum-knknkny <hutuuneko_mukiryoku@yahoo.co.jp>
2026-06-28 13:29:37 +0900

    novel.jsをnovelA.jsおよびnovelB.jsに分割し、novel-a.htmlおよびnovel-b.htmlでそれぞれのスクリプトを読み込むように変更

---
+++ js/novelA.js
@@ -0,0 +1,198 @@
+function getAFirstName() {
+    return localStorage.getItem("dreamAFirstName")
+        || "アカリ";
+}
+
+function getALastName() {
+    return localStorage.getItem("dreamALastName")
+        || "ユウキ";
+}
+
+function saveAFirstName() {
+
+    const value =
+        document
+        .getElementById("AFirstNameInput")
+        .value
+        .trim();
+
+    localStorage.setItem(
+        "dreamAFirstName",
+        value
+    );
+}
+
+function saveALastName() {
+
+    const value =
+        document
+        .getElementById("ALastNameInput")
+        .value
+        .trim();
+
+    localStorage.setItem(
+        "dreamALastName",
+        value
+    );
+}
+
+function toggleChapter(id) {
+
+    const element =
+        document.getElementById(id);
+
+    if (element.style.display === "none") {
+
+        element.style.display = "block";
+
+    } else {
+
+        element.style.display = "none";
+
+    }
+}
+
+function generateStoryList() {
+
+    const chapters = {};
+
+    storyList.forEach(story => {
+
+        if (!chapters[story.chapter]) {
+
+            chapters[story.chapter] = {
+                name: story.chapterName,
+                stories: []
+            };
+        }
+
+        chapters[story.chapter]
+            .stories
+            .push(story);
+    });
+
+    const container =
+        document.getElementById("storyList");
+
+    container.innerHTML = "";
+
+    Object.keys(chapters).forEach(chapterNumber => {
+
+        const chapter =
+            chapters[chapterNumber];
+
+        let storyButtons = "";
+
+        chapter.stories.forEach(story => {
+
+            storyButtons += `
+                <button
+                    onclick="openStory('${story.id}')"
+                >
+                    ${story.title}
+                </button>
+                <br>
+            `;
+        });
+
+        container.innerHTML += `
+            <button class="chapter"
+                onclick="
+                    toggleChapter(
+                        'chapter-${chapterNumber}'
+                    )
+                "
+            >
+                ${chapter.name}
+            </button>
+
+            <div
+                id="chapter-${chapterNumber}"
+                class="chapter-stories"
+                style="display:none;"
+            >
+                ${storyButtons}
+            </div>
+
+            <br>
+        `;
+    });
+}
+
+async function openStory(id) {
+
+    console.log("開こうとしてる:", id);
+
+    const path =
+        `content/novel-a/${id}.md`;
+
+    console.log("パス:", path);
+
+    const response =
+        await fetch(path);
+
+    console.log(response.status);
+
+    const markdown =
+        await response.text();
+
+    const parts =
+        markdown.split("---");
+
+    const frontMatter =
+        parts[1];
+
+    let content =
+        parts[2];
+
+    const titleMatch =
+        frontMatter.match(
+            /title:\s*(.*)/
+        );
+
+    document
+        .getElementById("storyTitle")
+        .textContent =
+        titleMatch[1];
+
+    content =
+        content
+        .replaceAll(
+            "<<ナマエ>>",
+            getAFirstName()
+        )
+        .replaceAll(
+            "<<ミョウジ>>",
+            getALastName()
+        );
+
+    document
+        .getElementById("novel")
+        .textContent =
+        content;
+
+    document
+        .getElementById("home")
+        .style.display =
+        "none";
+
+    document
+        .getElementById("storyPage")
+        .style.display =
+        "block";
+}
+
+function backToList() {
+
+    document
+        .getElementById("storyPage")
+        .style.display =
+        "none";
+
+    document
+        .getElementById("home")
+        .style.display =
+        "block";
+}
+
+generateStoryList();
