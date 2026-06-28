f094462561512610a04ef1ea17fb27f11370e9b3
baum-knknkny <hutuuneko_mukiryoku@yahoo.co.jp>
2026-06-28 14:59:53 +0900

    getItemを使用してgenerateMarkdown関数内の変数取得を整理

--- js/admin.js
+++ js/admin.js
@@ -1,10 +1,31 @@
 function generateMarkdown() {
-const title = document.getElementById("title").value;
-const chapter = document.getElementById("chapter").value;
-const chapterName = document.getElementById("chapterName").value;
-const episode = document.getElementById("episode").value;
-const summary = document.getElementById("summary").value;
-const body = document.getElementById("body").value;
+function getTitle (title) {
+    const value = document.getItem("title").value.trim();
+    return value;
+}
+function getChapter (chapter) {
+    const value = document.getItem("chapter").value.trim();
+    return value;
+}
+function getChapterName (chapterName) {
+    const value = document.getItem("chapterName").value.trim();
+    return value;
+}
+function getEpisode (episode) {
+    const value = document.getItem("episode").value.trim();
+    return value;
+}
+function getSummary (summary) {
+    const value = document.getItem("summary").value.trim();
+    return value;
+}
+const title = getTitle(1);
+const chapter = getChapter(1);
+const chapterName = getChapterName(1);
+const episode = getEpisode(1);
+const summary = getSummary(1);
+const body = document.getElementById("body").value.trim();
+const today = new Date().toISOString().split("T")[0];
 const markdown = `---
 title: ${title}
 
