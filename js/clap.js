af74d3407d44f32a6ae3d203ae5e358c46ff1976
baum-knknkny <hutuuneko_mukiryoku@yahoo.co.jp>
2026-06-30 00:06:22 +0900

    Update clap.html and clap.js: Modify message input and enhance clap functionality

---
+++ js/clap.js
@@ -0,0 +1,77 @@
+async function sendMessage() {
+
+    const message =
+        document
+        .getElementById("message")
+        .value
+        .trim;
+
+    await sendToDiscord(message);
+    showRandomClap();
+    
+}
+
+async function showRandomClap() {
+const randomID =
+    clapList[
+        Math.floor(Math.random() * clapList.length)
+    ];
+
+
+const response =
+    await fetch(
+        `content/clap/${randomID}.md`
+    );
+
+const markdown =
+    await response.text();
+
+const parts =
+    markdown.split("---");
+
+const frontMatter =
+    parts[1];
+    
+const content =
+    parts[2];
+
+const title =
+    frontMatter.match(
+        /title:\s*(.*)/
+    )[1];
+
+document
+    .getElementById("clapTitle")
+    .textContent =
+    title;
+
+document
+    .getElementById("clapStory")
+    .innerHTML =
+    content;
+
+document
+    .getElementById("clapArea")
+    .style.display =
+    "block";
+    }
+
+async function sendToDiscord(message) {
+    await fetch(
+        "https://clap.hutuuneko-mukiryoku.workers.dev",
+    {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            message: message,
+            page: location.pathname
+        })
+    })
+    if (message === "") {
+        // "👏"
+    } else {
+        // "💌"
+    }
+}
