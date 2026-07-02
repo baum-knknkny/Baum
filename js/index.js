43a8f7681dd768961b6779e854e4cc477bc10d0b
baum-knknkny <hutuuneko_mukiryoku@yahoo.co.jp>
2026-07-02 15:34:33 +0900

    訪問者数を増加させるための非同期関数increaseVisitorを追加

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
+async function increaseVisitor() {
+
+    const response = await fetch(
+        "https://counter-woker.hutuuneko-mukiryoku.workers.dev/",
+        {
+            method: "POST"
+        }
+    );
+
+    const data = await response.json();
+
+    document
+        .getElementById("visitorCount")
+        .textContent =
+        data.count;
+
+}
+
 increaseVisitor();
+
