29f0a73ca88a4eaef1cefd9fbc5ec8a4e92d0250
baum-knknkny <hutuuneko_mukiryoku@yahoo.co.jp>
2026-08-20 23:19:25 +0900

    update: refactor story button generation in generateStoryList function

--- js/novelA.js
+++ js/novelA.js
@@ -90,30 +90,17 @@ function generateStoryList() {
 
         let storyButtons = "";
 
-chapter.stories.forEach(story => {
+        chapter.stories.forEach(story => {
 
-    storyButtons += `
-        <div class="story-list-row">
-
-            <button
-                class="color-button"
-                onclick="openStory('${story.id}')"
-            >
-                ${story.title}
-            </button>
-
-            <span class="story-list-like">
-                <imgsrc = "../../image/pinkhart.png">
-                <span
-                    data-story-list-like="${story.id}"
+            storyButtons += `
+                <button class="color-button" 
+                    onclick="openStory('${story.id}')"
                 >
-                    0
-                </span>
-            </span>
-
-        </div>
-    `;
-});
+                    ${story.title}
+                </button>
+                <br>
+            `;
+        });
 
         container.innerHTML += `
             <button class="chapter"
